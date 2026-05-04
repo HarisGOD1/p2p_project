741318875245aacd08e61c11d0db00223ffe5fac
thegod <kamil29june@yandex.ru>
2026-05-04 13:27:59 +0300

    + License

--- buildSrc/settings.gradle.kts
+++ buildSrc/settings.gradle.kts
@@ -9,7 +9,11 @@ dependencyResolutionManagement {
     // Reuse the version catalog from the main build.
     versionCatalogs {
         create("libs") {
-            from(files("../gradle/libs.versions.toml"))
+            from(
+                files(
+                    "../gradle/libs.versions.toml"
+                )
+            )
         }
     }
 }
